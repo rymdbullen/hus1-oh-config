125846064d761e39187e244f137711cca754796b
Jan Stenvall <jan.stenvall@gmail.com>
2015-08-11 16:12:18 +0200

    fixing mqtt addons, adding humidex

---
+++ jdk_dl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# this script needs a config file with the following parameters:
+#
+
+JAVA_MAJOR_VERSION=8
+JAVA_SECURITY_VERSION=23
+JAVA_UPDATE=6
+JAVA_ARCH=arm-vfp-hflt
+
+RUN pacman -Suy --noconfirm
+
+wget -O /tmp/jdk-linux.tar.gz \
+--no-cookies \
+--no-check-certificate \
+--header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u33-b05/jdk-8u33-linux-arm-vfp-hflt.tar.gz"
+
+# tar creates /opt/jdk1.8.0_06
+tar -zxC /opt -f /tmp/jdk-linux.tar.gz
+ln -s /opt/jdk1.8.0_0${JAVA_UPDATE} /opt/jdk8
+~                  
